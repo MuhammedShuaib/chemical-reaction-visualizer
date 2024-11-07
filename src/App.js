b15dbb0370b39189ba2814913fa9325563e0cc28
shuaib <muhammedshuaibkhan03@gmail.com>
2024-11-07 19:35:47 +0600

    Initial commit - setup React project structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chemical Reaction Visualization App</h1>
+      {/* Components will go here */}
+    </div>
+  );
+}
+
+export default App;
